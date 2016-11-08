df3bdf25e467db5a031e7c385f8fc3f7c983426f
jessalbarian <albarian.jess@gmail.com>
2016-11-07 17:57:03 -0700

    added atlas logo

--- website/index.php
+++ website/index.php
@@ -414,9 +414,14 @@
 
                     </div>
                     <div class="row">
-                        <div class="col-lg-12 mar">
+                        <div class="col-lg-6 mar">
                             <a href="https://www.soylent.com/" target="_blank">
                                 <img src="img/partners/soylent-logo.png">
+                            </a>
+                        </div>
+                        <div class="col-lg-6 mar">
+                            <a href="http://atlas.colorado.edu/" target="_blank">
+                                <img src="img/partners/atlas.jpg">
                             </a>
                         </div>
                     </div>
