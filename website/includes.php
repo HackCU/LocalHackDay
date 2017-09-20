9154f3b648aae1bf2c4e07c511faeef8df3e0b47
Apara Venkat <ssapar@gmail.com>
2017-09-19 23:18:55 -0600

    Purge 'stellar' animation files

--- website/includes.php
+++ website/includes.php
@@ -2,14 +2,12 @@
 <script src="inc/jquery/jquery-1.11.1.min.js"></script>
 <script src="inc/bootstrap/js/bootstrap.min.js"></script>
 <script src="inc/owl-carousel/js/owl.carousel.min.js"></script>
-<script src="inc/stellar/js/jquery.stellar.min.js"></script>
 <script src="inc/waypoints.min.js"></script>
 <script src="inc/isotope.pkgd.min.js"></script>
 <script src="inc/classie.js"></script>
 <script src="inc/jquery.easing.min.js"></script>
 <script src="inc/jquery.counterup.min.js"></script>
 <script src="inc/smoothscroll.js"></script>
-
 
 <!-- Theme JS -->
 <script src="js/theme.js"></script>
